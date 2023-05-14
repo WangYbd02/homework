6d840698ba423d824e9d8857de55400a128348cf
WangYbd02 <129845947+WangYbd02@users.noreply.github.com>
2023-05-14 17:25:21 +0800

    Add files via upload

---
+++ DSP/MFCC/extract_MFCC.py
@@ -0,0 +1,107 @@
+"""
+@ 提取MFCC参数
+"""
+
+
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+from preprocessing import read_data, preEmphasis, enframed
+from scipy.fftpack import dct
+from VAD import point_check
+
+
+def extractMFCC(frames, framerate, NFFT, n_filters=24, num_ceps=12):
+    """
+    提取MFCC系数
+    :param frames: 2-dim ndarray. 经过预加重、分帧、加窗处理后的信号数据，大小为(帧数，帧长)
+    :param framerate: int. 信号的采样频率
+    :param NFFT: int. 作fft的点数，一般取值为帧长且为2的幂次
+    :param n_filters: int. mel滤波器的数目
+    :param num_ceps: int. 选取的MFCC系数的阶数
+    :return: 2-dim ndarrat. 每一帧的MFCC系数，大小为(帧数，num_ceps)
+    """
+    num_frames = frames.shape[0]
+    mag = np.zeros(frames.shape, dtype=complex)
+    power = np.zeros(frames.shape)
+    for i in range(num_frames):
+        mag[i, :] = np.fft.fft(frames[i, :], NFFT)
+        power[i, :] = np.square(abs(mag[i, :]))/(NFFT/2)**2
+        power[i, 0] = power[i, 0]/4
+    # plt.plot(np.arange(256), power[3, :], color='r')
+    # plt.show()
+    low_freq_mel = 0  # mel最低频率
+    high_freq_mel = 2595*np.log10(1+(framerate/2)/700)  # mel最高频率
+    mel_points = np.linspace(low_freq_mel, high_freq_mel, n_filters+2)  # 将mel频率等距分点
+    hz_points = 700*(10**(mel_points/2595)-1)  # 将等距分好的mel频率转换为实际频率
+    w2 = int(NFFT/2+1)  # 有效点数
+    df = framerate/(NFFT+1)  # fs/N
+    s = np.zeros((num_frames, n_filters))  # mel滤波器能量
+    color = ['b', 'g', 'r', 'm', 'orange', 'k']
+    freq = []  # 频率
+    for n in range(0, w2):
+        freqs = int(n*df)
+        freq.append(freqs)
+    bank = np.zeros((n_filters, w2))  # mel滤波器的频率响应
+    for m in range(1, n_filters+1):
+        m0 = int(np.floor(hz_points[m]/df))  # 第m个mel滤波器的中心频率点
+        m1 = int(np.floor(hz_points[m-1]/df))  # 第m-1个mel滤波器的中心频率点
+        m2 = int(np.floor(hz_points[m+1]/df)+1)  # 第m+1个mel滤波器的中心频率点
+        for k in range(m1, m0):
+            bank[m-1, k] = (k-m1)/(m0-m1)
+        for k in range(m0, m2):
+            bank[m-1, k] = (m2-k)/(m2-m0)
+    #     plt.plot(freq, bank[m-1, :], color=color[(m-1)%6])  # 画出mel滤波器组
+    #     # plt.plot((0, 4000), (0, 0), 'k')
+    #     # plt.plot((0, 4000), (1, 1), 'k')
+    #     # plt.xlabel('Frequency')
+    #     # plt.ylabel('Amplitude')
+    #     # plt.show()
+    s = np.dot(power[:, :w2], bank.T)  # mel滤波器能量
+    # plt.imshow(np.flip(s.transpose(1, 0), axis=0), cmap='hot')
+    # plt.xlabel('Frames')
+    # plt.ylabel('Frequency/kHz')
+    # plt.yticks(np.arange(0, 45/40*26, 5/40*26), np.arange(40, -1, -5)/10)
+    # plt.show()
+    logs = np.log(s)  # mel滤波器能量取e为底对数
+    mfcc0 = dct(logs, type=2, axis=1, norm='ortho')[:, 1:(num_ceps+1)]  # 离散余弦变换后得MFCC并取低频的num_ceps项
+    # plt.imshow(np.flip(mfcc0.transpose(1, 0), axis=0), cmap='hsv')
+    # plt.xlabel('Frames')
+    # plt.ylabel('MFCC Coefficients')
+    # plt.yticks(np.arange(0, 12, 11), np.arange(13, 1, -11))
+    # plt.show()
+    mfcc1_start = mfcc0[:2, :].copy()
+    mfcc1_end = mfcc0[-2:, :].copy()
+    mfcc1_mid = (2*mfcc0[4:, :]+mfcc0[3:-1, :]-2*mfcc0[:-4, :]-mfcc0[1:-3, :])/10
+    mfcc1 = np.concatenate((mfcc1_start, mfcc1_mid, mfcc1_end), axis=0)  # 一阶差分
+    mfcc2_start = mfcc1[:2, :].copy()
+    mfcc2_end = mfcc1[-2:, :].copy()
+    mfcc2_mid = (2*mfcc1[4:, :]+mfcc1[3:-1, :]-2*mfcc1[:-4, :]-mfcc1[1:-3, :])/10
+    mfcc2 = np.concatenate((mfcc2_start, mfcc2_mid, mfcc2_end), axis=0)  # 二阶差分
+    mfcc = np.concatenate((mfcc0, mfcc1, mfcc2), axis=1)
+    # plt.imshow(np.flip(mfcc.transpose(1, 0), axis=0), cmap='hsv')
+    # plt.xlabel('Frames')
+    # plt.ylabel('MFCC Coefficients')
+    # plt.yticks(np.arange(0, 36, 12), np.arange(36, 0, -12))
+    # plt.show()
+    return mfcc
+
+
+if __name__ == '__main__':
+    data_path = 'D:/dataset/voice dataset/0/80-0-0.wav'
+    win_len = 256
+    step_len = 128
+    n_filters = 26  # mel滤波器的个数
+    wave_data, nframes, framerate = read_data(data_path)
+    # wave_data = wave_data[:3*framerate]
+    # nframes = 3*framerate
+    # time_list = np.array(range(0, nframes)) * (1.0/framerate)
+    emphasized_signal = preEmphasis(wave_data, 0.97)
+    # plt.plot(time_list, emphasized_signal)
+    # plt.show()
+    frames = enframed(emphasized_signal, win_len, step_len, win_method=1)  # (186, 256)
+    StartPoint, EndPoint = point_check(frames)  # 端点检测
+    framed_voice = frames[StartPoint:EndPoint, :]  # 分帧后的语音段
+    NFFT = win_len
+    mfcc = extractMFCC(framed_voice, framerate, NFFT, n_filters)
+    print(mfcc)
---
+++ DSP/MFCC/preprocessing.py
@@ -0,0 +1,82 @@
+"""
+@ 语音信号预处理
+"""
+
+
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+
+
+def read_data(data_path):
+    """读取语音信号"""
+    f = wave.open(data_path)
+    params = f.getparams()
+    # 声道数，量化位数，采样率，采样点数
+    nchannels, sampwidth, framerate, nframes = params[:4]
+    str_data = f.readframes(nframes)  # 读取音频，字符串格式
+    f.close()
+    wave_data = np.fromstring(str_data, dtype=np.short)  # 将字符串转换为浮点型数据
+    wave_data = wave_data/max(abs(wave_data))  # wave幅值归一化
+    return wave_data, nframes, framerate
+
+def preEmphasis(wave_data, a):
+    """语音信号预加重"""
+    res = np.append(wave_data[0], wave_data[1:]-a*wave_data[:-1])
+    return res
+
+def win_func(win_len, n, method=0):
+    """窗函数"""
+    if method == 0:  # 矩形窗
+        return 1
+    elif method == 1:  # 汉明窗
+        return 0.54-0.46*np.cos(2*np.pi*n/(win_len-1))
+    elif method == 2:  # 海宁窗
+        return 0.5*(1-np.cos(2*np.pi*n/(win_len-1)))
+
+def enframed(data, win_len, step_len, win_method=0):
+    """分帧和加窗"""
+    data_len = len(data)
+    num_frames = int(np.floor((data_len-win_len)/step_len)+1)  # 总帧数
+    framed = np.zeros((num_frames, win_len))
+    for i in range(num_frames):
+        for j in range(win_len):
+            framed[i, j] = data[i*step_len+j] * win_func(win_len, j, win_method)
+    return framed
+
+
+if __name__ == '__main__':
+    data_path = './voice_data/OSR_us_000_0010_8k.wav'
+    win_len = 256
+    step_len = 128
+    wave_data, nframes, framerate = read_data(data_path)
+    wave_data = wave_data[:3*framerate]
+    nframes = 3*framerate
+    time_list = np.array(range(0, nframes)) * (1.0 / framerate)
+    wave_data_after = preEmphasis(wave_data, 0.97)
+    plt.plot(time_list, wave_data_after, 'b')
+    plt.grid()
+    plt.show()
+    framed = enframed(wave_data_after, win_len, step_len, 1)
+    mag = np.zeros(framed.shape, dtype=complex)  # 频谱
+    energy = np.zeros(framed.shape)  # 谱线能量
+    for i in range(framed.shape[0]):
+        mag[i, :] = np.fft.fft(framed[i, :], win_len)
+        energy[i, :] = np.square(abs(mag[i, :]))
+    print(energy.shape)
+    # plt.plot(mag[0, :])
+    # plt.plot(np.arange(win_len), energy[0, :])
+    plt.plot(np.arange(256), framed[78])
+    # plt.plot(time_list, wave_data_after, color='b')
+    # plt.plot((1.2, 1.2), (-0.2, 0.2), color='r')
+    # plt.plot((1.232, 1.232), (-0.2, 0.2), color='r')
+    # plt.plot((1.2, 1.232), (-0.2, -0.2), color='r')
+    # plt.plot((1.2, 1.232), (0.2, 0.2), color='r')
+    #
+    # plt.plot((1.216, 1.216), (-0.25, 0.25), color='k')
+    # plt.plot((1.248, 1.248), (-0.25, 0.25), color='k')
+    # plt.plot((1.216, 1.248), (-0.25, -0.25), color='k')
+    # plt.plot((1.216, 1.248), (0.25, 0.25), color='k')
+    plt.grid()
+    plt.show()
